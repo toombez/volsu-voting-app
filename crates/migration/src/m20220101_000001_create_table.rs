966b5157150bcb00220ea01adab79200f490ce10
Timur <toombez.timurtokaev@gmail.com>
2025-05-02 20:47:36 +0300

    Add migrations for user

---
+++ crates/migration/src/m20220101_000001_create_table.rs
@@ -0,0 +1,41 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(User::Table)
+                    .if_not_exists()
+                    .col(pk_uuid(User::Id))
+                    .col(string(User::Username))
+                    .col(string(User::Password))
+                    .col(string(User::Status))
+                    .to_owned()
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(
+                Table::drop()
+                    .table(User::Table)
+                    .to_owned()
+                )
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum User {
+    Table,
+    Id,
+    Username,
+    Password,
+    Status,
+}
