9c4f16abee012dd6064a36f97d93a8e28676585f
Timur <toombez.timurtokaev@gmail.com>
2025-05-05 02:09:10 +0300

    Add cors to backend

--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -1,6 +1,7 @@
-use axum::Router;
+use axum::{http::Method, Router};
 use backend::{app_state::AppState, routers};
 use migration::{Migrator, MigratorTrait};
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
@@ -14,8 +15,13 @@ async fn main() {
 
     let app_state = AppState { connection };
 
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST, Method::PUT, Method::PATCH])
+        .allow_origin(Any);
+
     let app = Router::new()
         .merge(routers::create_router(app_state.clone()))
+        .layer(cors)
         .with_state(app_state.clone());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
