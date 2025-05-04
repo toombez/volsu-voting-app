4f182f3de7fa6234fe5a91e26caa3d0d97fb2610
Timur <toombez.timurtokaev@gmail.com>
2025-05-04 14:03:11 +0300

    Fix state in routers

--- apps/backend/src/routers/api/v1/mod.rs
+++ apps/backend/src/routers/api/v1/mod.rs
@@ -1,13 +1,14 @@
 mod auth;
 mod users;
 mod votings;
+pub mod middleware;
 
 use axum::Router;
 use crate::app_state::AppState;
 
-pub fn create_router() -> Router<AppState> {
+pub fn create_router(state: AppState) -> Router<AppState> {
     Router::new()
-        .nest("/auth", auth::create_router())
-        .nest("/users", users::router::create_router())
-        .nest("/votings", votings::create_router())
+        .nest("/auth", auth::router::create_router(state.clone()))
+        .nest("/users", users::router::create_router(state.clone()))
+        .nest("/votings", votings::create_router(state.clone()))
 }
