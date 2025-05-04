ef021279b090e3c6e6749154b5a2aeaf87f24791
Timur <toombez.timurtokaev@gmail.com>
2025-05-04 15:54:21 +0300

    Return author, votes_count in get voting by id

--- apps/backend/src/routers/api/v1/votings/handlers/get_voting_by_id.rs
+++ apps/backend/src/routers/api/v1/votings/handlers/get_voting_by_id.rs
@@ -1,20 +1,14 @@
 use axum::{extract::{Path, State}, http::StatusCode, response::IntoResponse};
-use serde::{Deserialize, Serialize};
 use serde_json::json;
 use uuid::Uuid;
 
 use entity::voting::Entity as VotingEntity;
-use sea_orm::entity::*;
+use entity::user_vote::{Entity as UserVoteEntity, Column as UserVoteColumn};
+use entity::user::Entity as UserEntity;
+use sea_orm::{entity::*, PaginatorTrait, QueryFilter};
 
 use crate::{app_state::AppState, types::{ErrorResponse, SuccessResponse}};
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct PublicVoting {
-    id: Uuid,
-    title: String,
-    text: String,
-    author_id: Uuid
-}
+use crate::types::{PublicVoting, PublicVotingAuthor};
 
 pub async fn get_voting_by_id(
     Path(id): Path<Uuid>,
@@ -40,13 +34,51 @@ pub async fn get_voting_by_id(
         Some(voting) => voting,
     };
 
+    let author = UserEntity::find_by_id(voting.author_id)
+        .one(&state.connection)
+        .await;
+
+    let author = match author {
+        Err(_error) => return (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            ErrorResponse::new(json!("Internal server error"))
+        ).into_response(),
+        Ok(author) => author,
+    };
+
+    let author = match author {
+        None => return (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            ErrorResponse::new(json!("Internal server error"))
+        ).into_response(),
+        Some(author) => author,
+    };
+
+    let votes_count = UserVoteEntity::find()
+        .filter(UserVoteColumn::VotingId.eq(voting.id))
+        .count(&state.connection)
+        .await;
+
+    let votes_count = match votes_count {
+        Err(_error) =>  return (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            ErrorResponse::new(json!("Internal server error"))
+        ).into_response(),
+        Ok(votes_count) => votes_count,
+    };
+
     (
         StatusCode::OK,
         SuccessResponse::new(json!(PublicVoting {
-            author_id: voting.author_id,
+            created_at: voting.created_at,
             id: voting.id,
             text: voting.text,
             title: voting.title,
+            votes_count: votes_count as usize,
+            author: PublicVotingAuthor {
+                id: author.id,
+                username: author.username,
+            }
         }))
     ).into_response()
 }
