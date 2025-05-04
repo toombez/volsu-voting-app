60f130f53e619af4ef377b54803e513a4c95fe9d
Timur <toombez.timurtokaev@gmail.com>
2025-05-04 15:53:49 +0300

    Add model types for responses

--- apps/backend/src/types.rs
+++ apps/backend/src/types.rs
@@ -1,6 +1,12 @@
 use axum::{body::Body, http::Response, response::IntoResponse};
+use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
+use uuid::Uuid;
+
+// ============================================================================
+// API Responses
+// ============================================================================
 
 #[derive(Debug, Serialize, Clone, Deserialize)]
 pub struct SuccessResponse<D: Clone + Serialize> {
@@ -42,4 +48,40 @@ impl <E: Clone + Serialize> IntoResponse for ErrorResponse<E> {
     fn into_response(self) -> axum::response::Response {
         Response::new(Body::from(json!(self).to_string()))
     }
+}
+
+// ============================================================================
+// Types for public responses
+// ============================================================================
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PublicUserVoting {
+    pub id: Uuid,
+    pub title: String,
+    pub text: String,
+    pub created_at: NaiveDateTime,
+    pub votes_count: usize,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PublicUser {
+    pub id: Uuid,
+    pub username: Uuid,
+    pub votings: Vec<PublicUserVoting>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PublicVotingAuthor {
+    pub id: Uuid,
+    pub username: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PublicVoting {
+    pub id: Uuid,
+    pub title: String,
+    pub text: String,
+    pub created_at: NaiveDateTime,
+    pub author: PublicVotingAuthor,
+    pub votes_count: usize,
 }
