4a6895179bb7f4e175d1cd3c4ece06548550a0cc
Timur <toombez.timurtokaev@gmail.com>
2025-05-06 19:00:20 +0300

    Add error data

--- crates/types/src/api_response.rs
+++ crates/types/src/api_response.rs
@@ -1,6 +1,8 @@
 use std::fmt::Debug;
+use chrono::{NaiveDateTime, Utc};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
+use validator::{ValidationError, ValidationErrors};
 
 #[derive(Debug, Clone)]
 #[derive(Serialize, Deserialize)]
@@ -31,15 +33,23 @@ where D: Serialize {
     }
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ErrorApiResponse<E> {
-    pub status: String,
-    pub errors: Vec<E>,
+#[derive(Debug, Clone)]
+#[derive(Serialize, Deserialize)]
+#[derive(PartialEq, PartialOrd)]
+pub struct ErrorApiResponseData {
+    pub code: String,
+    pub message: Option<String>,
+    pub timestamp: NaiveDateTime,
 }
 
-impl <E> ErrorApiResponse<E>
-where E: Debug + Clone {
-    pub fn new(errors: &Vec<E>) -> Self {
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ErrorApiResponse {
+    pub status: String,
+    pub errors: Vec<ErrorApiResponseData>,
+}
+
+impl ErrorApiResponse {
+    pub fn new(errors: Vec<ErrorApiResponseData>) -> Self {
         Self {
             errors: errors.clone(),
             status: "error".to_string(),
@@ -47,13 +57,55 @@ where E: Debug + Clone {
     }
 }
 
-impl <E> Into<serde_json::Value>
-for ErrorApiResponse<E>
-where E: Serialize {
+impl Into<serde_json::Value>
+for ErrorApiResponse {
     fn into(self) -> serde_json::Value {
         json!({
             "status": self.status,
-            "errors": json!(self.errors),
+            "errors": self.errors,
         })
+    }
+}
+
+impl ErrorApiResponseData {
+    pub fn new(code: &str, message: &Option<String>) -> Self {
+        Self {
+            code: code.to_string(),
+            message: message.clone().map(|message| message.to_string()),
+            timestamp: Utc::now()
+                .naive_local()
+                .into()
+        }
+    }
+}
+
+impl From<ValidationError> for ErrorApiResponseData {
+    fn from(value: ValidationError) -> Self {
+        Self::new(
+            &value.code,
+            &value.message.map(|message| message.to_string())
+        )
+    }
+}
+
+impl From<ValidationErrors> for ErrorApiResponse {
+    fn from(value: ValidationErrors) -> Self {
+        let errors = value
+            .field_errors()
+            .iter()
+            .map(|(_field_name, error)| error
+                .iter()
+                .map(|error| ErrorApiResponseData::from(error.clone()))
+            )
+            .flatten()
+            .collect();
+
+        Self::new(errors)
+    }
+}
+
+impl From<ErrorApiResponseData> for ErrorApiResponse {
+    fn from(value: ErrorApiResponseData) -> Self {
+        Self::new(vec![value])
     }
 }
